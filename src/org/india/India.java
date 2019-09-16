a02fdc262da10ca7ef77eb25a0042c366ea83337
VidyaSullia <vidyashreedhara@gmail.com>
2019-09-16 22:55:48 +0530

    ok

--- src/org/india/India.java
+++ src/org/india/India.java
@@ -3,13 +3,13 @@ package org.india;
 import org.tamilnadu.Tamilnadu; //type class name Tamilnadu inside class and ctrl+space bar, then delete class name.
 
 public class India extends Tamilnadu {
-	public void India() {
+	public void Indian() {
 		System.out.println("Indian Languages");
 	}
 
 	public static void main(String[] args) {
 		India I = new India();
-		I.India();
+		I.Indian();
 		I.tamilLanguage();
 		I.malayalam();
 		I.telugu();
